5ff463d14fa77df6c55760ce6090593764acb0b0
dyjeong365 <dyjeong365@gmail.com>
2024-04-08 11:32:58 +0900

    initial commit

---
+++ src/main/java/com/dyjeong365/ip_address/IpAddressApplication.java
@@ -0,0 +1,13 @@
+package com.dyjeong365.ip_address;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAddressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpAddressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyjeong365/ip_address/IpAddressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyjeong365.ip_address;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpAddressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
