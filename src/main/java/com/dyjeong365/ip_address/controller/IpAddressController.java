921c0a43860b064f688dbf9035a1574d0ed52f66
dyjeong365 <dyjeong365@gmail.com>
2024-04-08 13:02:33 +0900

    fix: to work correctly in Beanstalk

--- src/main/java/com/dyjeong365/ip_address/controller/IpAddressController.java
+++ src/main/java/com/dyjeong365/ip_address/controller/IpAddressController.java
@@ -7,7 +7,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IpAddressController {
     @GetMapping("/ip")
-    public String getIpAddress(HttpServletRequest request) {
-        return "My IPv4 Address: " + request.getRemoteAddr();
+    String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if (ip == null) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null) {
+            ip = request.getRemoteAddr();
+        }
+        return "My IP Address: " + ip;
     }
 }
